fbb48f8f0fc21a80551ea3c7e0f97288fa32348d
史春阳 <shichunyang02@meituan.com>
2020-10-26 14:57:03 +0800

    ES get API

--- src/main/java/com/scy/es/EsClient.java
+++ src/main/java/com/scy/es/EsClient.java
@@ -4,6 +4,8 @@ import com.scy.core.StringUtil;
 import com.scy.core.format.MessageUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.elasticsearch.action.DocWriteRequest;
+import org.elasticsearch.action.get.GetRequest;
+import org.elasticsearch.action.get.GetResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
 import org.elasticsearch.action.support.WriteRequest;
@@ -46,6 +48,21 @@ public class EsClient {
             return indexResponse.getId();
         } catch (Exception e) {
             log.error(MessageUtil.format("ES index error", e, "index", index, "id", id, "json", json));
+            return StringUtil.EMPTY;
+        }
+    }
+
+    /**
+     * 根据id查询文档
+     */
+    public String get(String index, String id) {
+        GetRequest getRequest = new GetRequest(index, id);
+        try {
+            GetResponse getResponse = restHighLevelClient.get(getRequest, RequestOptions.DEFAULT);
+            log.info(MessageUtil.format("ES get response", "index", index, "id", id, "getResponse", getResponse));
+            return getResponse.getSourceAsString();
+        } catch (Exception e) {
+            log.error(MessageUtil.format("ES get error", e, "index", index, "id", id));
             return StringUtil.EMPTY;
         }
     }
