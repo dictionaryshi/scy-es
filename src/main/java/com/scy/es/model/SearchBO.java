05c31c26cf04c01556cb08f070af5991dc89444d
史春阳 <shichunyang02@meituan.com>
2020-10-28 11:38:25 +0800

    搜索API

---
+++ src/main/java/com/scy/es/model/SearchBO.java
@@ -0,0 +1,17 @@
+package com.scy.es.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * SearchBO
+ *
+ * @author shichunyang
+ * Created by shichunyang on 2020/10/28.
+ */
+@Getter
+@Setter
+@ToString
+public class SearchBO {
+}
